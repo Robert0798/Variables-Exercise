cbe51d2f34d6b4954bf8158d59961214f31c03ab
Robert Anderson <rganderson0798@gmail.com>
2024-10-10 10:16:26 -0700

    Interpolated Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+string name = "Robert";
+int age = 300;
+char middleInitial = 'G';
+bool isOver200  = true;
+double swagLevel = 22.3;
+decimal worth = 123.45M;
+
+Console.WriteLine($"My name is {name}, I am {age} years old. My middle inital's name is {middleInitial}. It is {isOver200} I am over 200 years old, and have a swag level of {swagLevel}. My worth is {worth}.");
